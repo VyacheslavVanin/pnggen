7faf1526a511ba4797c4132ff6c9f487c042942d
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2018-03-10 19:52:52 +0300

    Refactoring

--- myimage.py
+++ myimage.py
@@ -9,25 +9,28 @@ class MyImage:
         self.ctx     = cairo.Context(self.surface) 
         self.width   = width
         self.height  = height
-        self.draw_rectangle(0, 0, width, height, bgcolor)
+        self.rectangle(0, 0, width, height, bgcolor)
 
     def write_png(self, filename):
         self.surface.mark_dirty()
         self.surface.write_to_png(filename)
 
     def set_color(self, color):
+        if not color:
+            return
+        assert len(color) in (3, 4)
         if len(color) == 3:
             color = color + (1,)
-        self.ctx.set_source_rgba(color[0], color[1], color[2], color[3])
+        self.ctx.set_source_rgba(*color)
 
-    def draw_rectangle(self, x, y, width, height,
-                       color=(0, 0, 0, 1), filled=True):
+    def rectangle(self, x, y, width, height,
+                       color=None, filled=True):
         self.set_color(color)
-        self.ctx.rectangle(x,y,width,height)
+        self.ctx.rectangle(x, y, width, height)
         if filled:
             self.ctx.fill()
 
-    def draw_circle(self, x, y, radius, color=(0, 0, 0, 1), filled=True):
+    def circle(self, x, y, radius, color=None, filled=True):
         self.set_color(color)
         self.ctx.arc(x, y, radius, 0, math.pi * 2)
         if filled:
@@ -35,12 +38,12 @@ class MyImage:
         else:
             self.ctx.stroke()
 
-    def draw_line(self, points, color=(0, 0, 0, 1), filled=False):
+    def line(self, points, color=None, filled=False):
         self.set_color(color)
         first = points[0]
-        self.ctx.move_to(first[0], first[1])
+        self.ctx.move_to(*first)
         for p in points[1:]:
-            self.ctx.line_to(p[0], p[1])
+            self.ctx.line_to(*p)
 
         if filled:
             self.ctx.close_path()
@@ -48,9 +51,10 @@ class MyImage:
         else:
             self.ctx.stroke()
 
-    def draw_text(self, text, pos=(5, 20), color=(0, 0, 0, 1), font_size=20):
+    def text(self, text, pos=(5, 20), color=None, font_size=20):
+        assert len(pos) == 2
         self.set_color(color);
-        self.ctx.move_to(pos[0], pos[1])
+        self.ctx.move_to(*pos)
         self.ctx.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
                                   cairo.FONT_WEIGHT_NORMAL)
         self.ctx.set_font_size(font_size)
@@ -59,12 +63,24 @@ class MyImage:
 
 def main():
     im = MyImage(256, 256)
-    im.draw_rectangle(100, 100, 56, 56, color=(1, 0, 0))
-    im.draw_circle(120, 100, 20, color=(0, 1, 0, 0.5));
+    #im.rectangle(100, 100, 56, 56, color=(1, 0, 0))
+    #im.circle(120, 100, 20, color=(0, 1, 0, 0.5));
 
+    #points = [(0, 0), (20, 30), (90, 5), (10, 30)]
+    #im.line(points, color=(0, 0, 1), filled=False)
+    #im.text("Hello", [1, 20])
+    im.set_color((1, 0, 0))
+    im.rectangle(100, 100, 56, 56)
+
+    im.set_color((0, 1, 0, 0.5))
+    im.circle(120, 100, 20);
+
+    im.set_color((1, 0, 0))
     points = [(0, 0), (20, 30), (90, 5), (10, 30)]
-    im.draw_line(points, color=(0, 0, 1), filled=False)
-    im.draw_text("Hello", [1, 20])
+    im.line(points, filled=False)
+
+    im.set_color((0, 0, 1))
+    im.text("Hello", [1, 20])
 
     im.write_png("test.png")
 
